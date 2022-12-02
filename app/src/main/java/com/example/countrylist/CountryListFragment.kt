8b3180bb6a841ae0cf01b1569c8d9973e080a129
Patrick Alexis <alexispatrick77@gmail.com>
2022-12-01 23:53:14 -0800

    Refactory country list fragment UI

--- app/src/main/java/com/example/countrylist/CountryListFragment.kt
+++ app/src/main/java/com/example/countrylist/CountryListFragment.kt
@@ -5,15 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import com.example.countrylist.databinding.FragmentFirstBinding
+import com.example.countrylist.databinding.FragmentCountryListBinding
 
-class FirstFragment : Fragment() {
+class CountryListFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentCountryListBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -21,7 +18,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentCountryListBinding.inflate(inflater, container, false)
         return binding.root
 
     }
